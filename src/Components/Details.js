8511a04a30b726bd1b31b3248c7a3d8261df441e
Sarah Guidry <sarahbethguidry@gmail.com>
2021-04-02 06:54:26 -0500

    updated styles

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
+import styled from 'styled-components'
 
 export const Details = props => {
   const { title, description, mediaType, url } = props
 
   const media =
-    { mediaType } === 'image' ? (
-      <img className='media' src={url} alt={title} />
-    ) : (
-         <ReactPlayer className='media' url={url} />
+    { mediaType } === 'video' ? (
+       <ReactPlayer className='media' url={url} />
+    ) : (<img className='media' src={url} alt={title} />
     )
-    const pStyles ={
-      margin:'50px',
-      fontSize:'1.5rem',
-      textAlign: 'center',
-      fontFamily: 'Megrim'
-    }
+const WrapperDiv = styled.div`
+    width: 100%;
+    height: 100%;
+`;
+
+    // const pStyles = styled.p`
+    //   margin:50px;
+    //   font-size:1.5rem;
+    //   text-align: center;
+    //   font-family: Megrim;
+    // `;
+
     const titleStyle = {
       margin:'20px',
       fontSize:'2rem',
@@ -32,9 +38,10 @@ export const Details = props => {
   return (
     <div style={titleStyle}className='details'>
       <h3 style={titleStyle}>{title}</h3>
-      <p style={pStyles}>{description}</p>
-      <div className='media-wrapper videoStyle'></div>
-      <div style={videoStyle} className={mediaType === 'image' ? 'image' : 'video'}>{media}</div>
-    </div>
+      <p>{description}</p>
+      <WrapperDiv>
+         <div style={videoStyle} className={mediaType === 'image' ? 'image' : 'video'}>{media}</div>
+      </WrapperDiv>
+  </div>
   )
 }
